75ffae9c285b2246ddb04a239739479986e1942c
tangxiao <1037706321@qq.com>
2023-06-06 00:18:59 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+const str ='ssss';
+
+</script>
+
+<template>
+  <div class="box">
+    <h1>APP</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.box{
+  background-color: aqua;
+  h1{}
+}
+</style>
